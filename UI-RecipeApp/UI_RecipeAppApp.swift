696793d7a6b15b097b127a790c36f46f0b7d49de
Valentin Malissen <keisuke.tk.fd@gmail.com>
2020-07-17 20:35:41 -0400

    Initial Commit

---
+++ UI-RecipeApp/UI_RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_RecipeAppApp.swift
+//  UI-RecipeApp
+//
+//  Created by Valentin Malissen on 2020-07-17.
+//
+
+import SwiftUI
+
+@main
+struct UI_RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
